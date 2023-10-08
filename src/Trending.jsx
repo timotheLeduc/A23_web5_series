414013a32ff9f9839ae62ea2f3f428343e09baca
timotheLeduc <2149272@cstj.qc.ca>
2023-10-08 09:56:48 -0400

    FixTrendingApi

--- src/Trending.jsx
+++ src/Trending.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ListeSeries from './ListeSeries';
 import { Outlet } from 'react-router-dom';
+import { useState , useEffect } from 'react';
 
 function Trending({
   seriesData,
@@ -16,11 +17,24 @@ function Trending({
   console.log('seriesData:', seriesData);
   console.log('seriesDetailsData:', seriesDetailsData);
 
+  const [series, setSeries] = useState([]);
+    useEffect(() => {
+      const fetchSeries = async() => {
+        const resp = await fetch('http://localhost:3000/api/series/trending');
+        const data = await resp.json();
+        setSeries(data.series);
+        console.log(data.series);
+      };
+
+      fetchSeries();
+    }, []);
+
+
   return (
     <div>
       <h2>Trending TV Series</h2>
       <ListeSeries
-        seriesData={seriesData}
+        seriesData={series}
         seriesDetailsData={seriesDetailsData}
         addToFavorites={addToFavorites}
         removeFromFavorites={removeFromFavorites}

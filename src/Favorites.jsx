1f6723ce6245583094b6cb26af439f37ce71c7f4
timotheLeduc <2149272@cstj.qc.ca>
2023-10-12 12:10:05 -0400

    justecommitpourundo

--- src/Favorites.jsx
+++ src/Favorites.jsx
@@ -4,6 +4,7 @@ import { Outlet } from 'react-router-dom';
 
 import { Link } from 'react-router-dom';
 import { useState , useEffect } from 'react';
+import ListeSeriesFavorites from './ListeSeriesFavorites';
 
 function Favorites({
   seriesData,
